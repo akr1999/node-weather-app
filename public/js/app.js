59a07afea0408c8813e53ff065e971302b51a1f3
akr1999 <ayushkumarrathore@gmail.com>
2019-05-31 13:08:58 +0530

    Changes in p block for good gui

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     messageOne.textContent = "Finding Weather..."
-    messageTwo.textContent = ""
+    messageTwo.textContent = "Loading..."
     const location = search.value 
     fetch("http://localhost:3000/weather?address="+location).then((response) => {
         response.json().then((data) => {
